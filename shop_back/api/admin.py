f9ae37343f6cb07d7bacc3bced071d82f183deca
Karina <karinakelic@gmail.com>
2025-02-14 21:47:29 +0600

    Delete multiple lab folders

--- shop_back/api/admin.py
+++
@@ -1,8 +0,0 @@
-# api/admin.py
-
-from django.contrib import admin
-from .models import Product, Category
-
-# Register Product and Category models to be managed through the Admin panel
-admin.site.register(Product)
-admin.site.register(Category)
