22b8bc27d874ca7a18586f6cbfed90a6f08cb76e
Karina <karinakelic@gmail.com>
2025-04-03 15:42:23 +0600

    Add lab7 folder, delete old files

--- codingbat/warmup-1/sleep_in.py
+++
@@ -1,7 +0,0 @@
-def sleep_in(weekday, vacation):
-    return not weekday or vacation
-
-# Test cases
-print(sleep_in(False, False))  # Should print: True
-print(sleep_in(True, False))   # Should print: False
-print(sleep_in(False, True))   # Should print: True
--- codingbat/warmup-2/string_times.py
+++
@@ -1,8 +0,0 @@
-def string_times(str, n):
-  result = ""
-  for i in range(n):  # range(n) is [0, 1, 2, .... n-1]
-    result = result + str  # could use += here
-  return result
-#string_times('Hi', 2) → 'HiHi'
-#string_times('Hi', 3) → 'HiHiHi'
-#string_times('Hi', 1) → 'Hi'
