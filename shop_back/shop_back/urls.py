56b940cb064b1dc61af705ee03e554200942bc4f
Karina <karinakelic@gmail.com>
2025-04-05 12:16:44 +0600

    Delete shop_back directory

--- shop_back/shop_back/urls.py
+++
@@ -1,9 +0,0 @@
-# shop_back/urls.py
-
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),  # Django Admin panel
-    path('api/', include('api.urls')),  # This includes the api URLs under /api/
-]
